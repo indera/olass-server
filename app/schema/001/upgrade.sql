f528f8ee07c03ae88da8bd111c7f6487d3e12f84
Andrei Sura <sura.andrei@gmail.com>
2016-04-25 09:20:05 -0400

    schema/001/upgrade.sql: use charset = utf-8 for database definition

--- app/schema/001/upgrade.sql
+++ app/schema/001/upgrade.sql
@@ -5,7 +5,7 @@ CREATE TABLE version (
    version_stamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    version_info text NOT NULL,
   PRIMARY KEY (version_id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO Version (version_id, version_stamp, version_info)
    VALUES('001', now(), 'New table: version')
