0dd07661b0c86e1f749db35f065363278a2e6bb1
Andrei Sura <sura.andrei@gmail.com>
2016-06-16 15:43:01 -0400

    Update tasks*.py for new invoke version

--- app/tasks.py
+++ app/tasks.py
@@ -5,90 +5,83 @@ Goal: store shortcuts to common tasks
     Andrei Sura <sura.andrei@gmail.com>
 
 """
-
-# import os
 import sys
-from invoke import run, task
+from invoke import task
 from tasks_utils import ask_yes_no, get_db_name, check_db_exists
-# from invoke.exceptions import Failure
-import colorama as col
-# @see https://pypi.python.org/pypi/colorama
-col.init(autoreset=True)
 
 STATUS_PASS = '✔'
 STATUS_FAIL = '✗'
 
 
 @task
-def list():
+def list(ctx):
     """ Show available tasks """
-    run('inv -l')
+    ctx.run('inv -l')
 
 
 @task
-def prep_develop():
+def prep_develop(ctx):
     """ Install the requirements """
-    run('pip install -r requirements.txt')
-    print(col.Fore.GREEN + "==> Pip packages installed:")
-    run('pip freeze')
+    ctx.run('pip install -r requirements.txt')
+    print("==> Pip packages installed:")
+    ctx.run('pip freeze')
 
 
 @task
-def init_db(db_name=None):
+def init_db(ctx, db_name=None):
     """ Create the database """
     db_name = db_name if db_name is not None else get_db_name()
     exists = check_db_exists(db_name)
 
     if exists:
-        print(col.Fore.RED +
-              "The database '{}' already exists "
+        print("The database '{}' already exists "
               "(name retrieved from schema/000/upgrade.sql)".format(db_name))
         sys.exit(1)
 
     if not ask_yes_no("Do you want to create the database '{}'?"
                       .format(db_name)):
-        print(col.Fore.YELLOW + "Aborting at user request.")
+        print("Aborting at user request.")
         sys.exit(1)
 
-    run('sudo mysql    < schema/000/upgrade.sql')
-    run('sudo mysql {} < schema/001/upgrade.sql'.format(db_name))
-    run('sudo mysql {} < schema/002/upgrade.sql'.format(db_name))
-    run('sudo mysql {} < schema/002/data.sql'.format(db_name))
-    run('sudo mysql {} < schema/003/upgrade.sql'.format(db_name))
-    run('sudo mysql {} < schema/003/data.sql'.format(db_name))
-    print(col.Fore.GREEN + "[{}] Done.".format(STATUS_PASS))
+    ctx.run('sudo mysql    < schema/000/upgrade.sql')
+    ctx.run('sudo mysql {} < schema/001/upgrade.sql'.format(db_name))
+    ctx.run('sudo mysql {} < schema/002/upgrade.sql'.format(db_name))
+    ctx.run('sudo mysql {} < schema/002/data.sql'.format(db_name))
+    ctx.run('sudo mysql {} < schema/003/upgrade.sql'.format(db_name))
+    ctx.run('sudo mysql {} < schema/003/data.sql'.format(db_name))
+    print("[{}] Done.".format(STATUS_PASS))
 
 
 @task
-def reset_db(db_name=None):
+def reset_db(ctx, db_name=None):
     """ Drop all tables, Create empty tables, and add data """
     db_name = db_name if db_name is not None else get_db_name()
 
     if not ask_yes_no("Do you want to erase the '{}' database"
                       " and re-create it?".format(db_name)):
-        print(col.Fore.YELLOW + "Aborting at user request.")
+        print("Aborting at user request.")
         sys.exit(1)
 
-    run('sudo mysql    < schema/000/downgrade.sql')
-    run('sudo mysql    < schema/000/upgrade.sql')
-    run('sudo mysql {} < schema/001/upgrade.sql'.format(db_name))
-    run('sudo mysql {} < schema/002/upgrade.sql'.format(db_name))
-    run('sudo mysql {} < schema/002/data.sql'.format(db_name))
-    run('sudo mysql {} < schema/003/upgrade.sql'.format(db_name))
-    run('sudo mysql {} < schema/003/data.sql'.format(db_name))
-    print(col.Fore.GREEN + "[{}] Done.".format(STATUS_PASS))
+    ctx.run('sudo mysql    < schema/000/downgrade.sql')
+    ctx.run('sudo mysql    < schema/000/upgrade.sql')
+    ctx.run('sudo mysql {} < schema/001/upgrade.sql'.format(db_name))
+    ctx.run('sudo mysql {} < schema/002/upgrade.sql'.format(db_name))
+    ctx.run('sudo mysql {} < schema/002/data.sql'.format(db_name))
+    ctx.run('sudo mysql {} < schema/003/upgrade.sql'.format(db_name))
+    ctx.run('sudo mysql {} < schema/003/data.sql'.format(db_name))
+    print("[{}] Done.".format(STATUS_PASS))
 
 
 @task(aliases=['run'])
-def go():
+def go(ctx):
     """
     Start the web application using the WSGI webserver provided by Flask
     """
-    run('python run.py')
+    ctx.run('python run.py')
 
 
 @task
-def show_versions(url='https://localhost'):
+def show_versions(ctx, url='https://localhost'):
     # run ('git fetch --tags')
 
     cmd = """git tag \
@@ -99,8 +92,8 @@ def show_versions(url='https://localhost'):
             | grep -oE "[0-9.]{1,2}[0-9.]{1,2}[0-9a-z.]{1,4}" \
             | tail -1"""
 
-    last_tag = run(cmd, pty=False, hide='both')
-    deployed_tag = run(cmd2, pty=False, hide='both')
+    last_tag = ctx.run(cmd, pty=False, hide='both')
+    deployed_tag = ctx.run(cmd2, pty=False, hide='both')
 
     print("Last tag in the repository: {}".format(last_tag.stdout.strip()))
     print("Deployed tag: {}".format(deployed_tag.stdout.strip()))
@@ -112,33 +105,33 @@ def show_versions(url='https://localhost'):
 
 
 @task
-def test():
+def test(ctx):
     """ Run tests """
-    run('PYTHONPATH="." py.test -v --tb=short -s tests/ --color=yes')
+    ctx.run('PYTHONPATH="." py.test -v --tb=short -s tests/ --color=yes')
 
 
 @task(aliases=['cov'])
-def coverage():
+def coverage(ctx):
     """ Create coverage report """
-    run('PYTHONPATH="." py.test --tb=short -s --cov olass '
-        ' --cov-report term-missing --cov-report html tests/')
-    run('open htmlcov/index.html')
+    ctx.run('PYTHONPATH="." py.test --tb=short -s --cov olass '
+            ' --cov-report term-missing --cov-report html tests/')
+    ctx.run('open htmlcov/index.html')
 
 
 @task
-def lint():
-    run("which pylint || pip install pylint")
-    run("pylint -f parseable olass")
+def lint(ctx):
+    ctx.run("which pylint || pip install pylint")
+    ctx.run("pylint -f parseable olass")
 
 
 @task
-def clean():
+def clean(ctx):
     """
     Remove all generated files
     """
-    run('find . -type f -name "*.pyc" -print | xargs rm -f')
-    run('rm -rf htmlcov/ .coverage pylint.out')
-    run('rm -rf .tox/*')
+    ctx.run('find . -type f -name "*.pyc" -print | xargs rm -f')
+    ctx.run('rm -rf htmlcov/ .coverage pylint.out')
+    ctx.run('rm -rf .tox/*')
 
 
 if __name__ == '__main__':
